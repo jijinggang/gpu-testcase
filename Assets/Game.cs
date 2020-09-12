c592215d5bdf4f7c633df7581b1013befdf51462
jijinggang <jijinggang@gmail.com>
2020-09-12 14:12:41 +0800

    自由修改图片大小/输出大小/压缩格式

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -4,86 +4,124 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    string[] TEST_NAME =
-    {
-        "1. 1000个512透明图片",
-        "2. 1000个128透明图片",
-        "3. 1000个128透明图片并放大4倍",
-        "4. 1000个用2张512透明图片交叉",
-    };
+
     // Start is called before the first frame update
-    GameObject prefab512, prefab512_2, prefab128;
-    int index = 0;
-    int count = 0;
+    GameObject prefab512;
+    Color32 COLOR = new Color32(255, 0, 0, 10);
+
+    int image_index = 0;
+    int output_index = 0;
+    int format_index = 0;
+    private Rect rectBtnImage = new Rect(0, 0, 200, 80);
+    private Rect rectBtnFormat = new Rect(220, 0, 200, 80);
+    private Rect rectBtnOutput = new Rect(440, 0, 200, 80);
+
+    private Rect rectBtnCreate = new Rect(700, 0, 80, 80);
+    private Rect rectBtnClear = new Rect(800, 0, 80, 80);
+    private Rect rectStatus = new Rect(0, 80, 300, 50);
     void Start()
     {
         prefab512 = Resources.Load<GameObject>("512");
-        prefab512_2 = Resources.Load<GameObject>("512-2");
-        prefab128 = Resources.Load<GameObject>("128");
+
+        //        style.fontSize = 20;
+
+        rectBtnClear.x = Screen.width - rectBtnCreate.width;
+
     }
 
-    private Rect rectBtnSwitch = new Rect(0, 0, 300, 80);
-    private Rect rectBtnCreate = new Rect(350, 0, 80, 80);
-    private Rect rectStatus = new Rect(450, 0, 300, 20);
+
     private int fps = 0;
+    private List<GameObject> objects = new List<GameObject>();
     // Update is called once per frame
+    float[] deltaTimes = new float[10] ;
+    int deltaIndex = 0;
     void Update()
     {
-        fps = Mathf.RoundToInt(1 / Time.deltaTime);
+        int len = deltaTimes.Length;
+        deltaIndex = (deltaIndex+1) % len;
+        deltaTimes[deltaIndex] = Time.deltaTime;
+
+        var sum = 0f;
+        for (int i = 0; i < len; i++)
+            sum += deltaTimes[i];
+
+        fps = Mathf.RoundToInt(len / sum);
     }
-    private void CreateObjs(int index)
+
+
+    int[] IMAGE_SIZES = { 256, 512, 1024 };
+    int[] OUTPUT_SIZES = { 256, 512, 1024 };
+    TextureFormat[] IMAGE_FORMATS = { TextureFormat.ARGB4444, TextureFormat.ARGB32, TextureFormat.ASTC_6x6, TextureFormat.ASTC_8x8 , TextureFormat.ASTC_HDR_8x8};
+    string[] IMAGE_FORMATS_STR = { "ARGB4444","ARGB32", "ASTC_6x6", "ASTC_8x8","ASTC_HDR_8x8" };
+
+    private void CreateObjects(int imageSize, int outputSize, TextureFormat format)
     {
+        var COLORS = new Color32[imageSize * imageSize];
+
+        for (int i = 0; i < COLORS.Length; i++)
+        {
+            COLORS[i] = COLOR;
+        }
+
+
         float z = 0f;
         for (int i = 0; i < 100; i++)
         {
             GameObject go = null;
-            if (index == 0)
-                go = Instantiate(prefab512);
-            else if (index == 1)
-                go = Instantiate(prefab128);
-            else if (index == 2)
-            {
-                go = Instantiate(prefab128);
-                go.transform.localScale = new Vector3(4, 4, 1);
-            }
-            else if (index == 3)
-            {
-                if (i % 2 == 0)
-                    go = Instantiate(prefab512);
-                else
-                    go = Instantiate(prefab512_2);
-            }
+            var texture = new Texture2D(imageSize, imageSize, format, false);
+            
+            //texture.SetPixels32(COLORS);
+            go = GameObject.Instantiate(prefab512);
+            var sp = go.GetComponent<SpriteRenderer>();
+            Sprite spt = Sprite.Create(texture, new Rect(0, 0, outputSize, outputSize), new Vector2(0.5f, 0.5f));
+            sp.sprite = spt;
+
+
             go.transform.Translate(0, 0, z);
             z += 0.01f;
 
-            count++;
+            objects.Add(go);
         }
     }
+    private void Clear()
+    {
+        foreach (var obj in objects)
+        {
+            Destroy(obj);
+        }
+        objects.Clear();
+    }
+
     void OnGUI()
     {
-
-        if (GUI.Button(rectBtnSwitch, TEST_NAME[index] + ":"))
+        GUI.skin.button.fontSize = 20;
+        GUI.skin.label.fontSize = 20;
+        if (GUI.Button(rectBtnImage, "图片:" + IMAGE_SIZES[image_index]))
         {
-            SelectNext();
+            image_index = (image_index + 1) % IMAGE_SIZES.Length;
         }
+        if (GUI.Button(rectBtnOutput, "输出:" + OUTPUT_SIZES[output_index]))
+        {
+            output_index = (output_index + 1) % OUTPUT_SIZES.Length;
+        }
+
+        if (GUI.Button(rectBtnFormat, "格式:" + IMAGE_FORMATS_STR[format_index]))
+        {
+            format_index = (format_index + 1) % IMAGE_FORMATS.Length;
+        }
+
         if (GUI.Button(rectBtnCreate, "Create"))
         {
-            CreateObjs(index);
+            CreateObjects(IMAGE_SIZES[image_index], OUTPUT_SIZES[output_index], IMAGE_FORMATS[format_index]);
         }
-        string status = "objects=" + count + ",fps=" + fps;
+        if (GUI.Button(rectBtnClear, "Clear"))
+        {
+            Clear();
+        }
+
+        string status = "objects=" + objects.Count + ",fps=" + fps;
         GUI.Label(rectStatus, status);
 
     }
-    private void Reset()
-    {
-    }
-    private void SelectNext()
-    {
-        var len = TEST_NAME.Length;
-        index++;
-        if (index >= len)
-        {
-            index = 0;
-        }
-    }
+
 }
